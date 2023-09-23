d84b7621d456ddcf03ef6dc1826c74eb9c8f710f
Agusti <me@agusti.me>
2023-09-23 07:44:13 +0200

    less is more v4.20

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com","s3-us-west-2.amazonaws.com","twitter.com"],
-  },
-  experimental: {
-    appDir: true,
-  },
+  }
 }
 
 export default withContentlayer(nextConfig)
