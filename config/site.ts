87230629e7e0a83dd07dc55086953d967acb6aec
Agusti <me@agusti.me>
2023-09-23 09:54:03 +0200

    fix og image

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "vvart.xyz",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  url: "https://vvart.vercel.app",
+  ogImage: "https://vvart.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/mattob_eth",
     github: "https://twitter.com/mattob_eth",
